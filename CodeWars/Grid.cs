af65577cd93290fe91cc9fecc39dfe1b8b9358cd
Tobie Allen <tallen@student.neumont.edu>
2022-03-30 14:14:00 -0600

    Number of Rectangles in a Grid Codewars finished

---
+++ CodeWars/Grid.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class Grid
+    {
+        public static int NumberOfRectangles(int m, int n)
+        {
+            return (m * n * (n + 1) * (m + 1)) / 4;
+        }
+    }
+}
