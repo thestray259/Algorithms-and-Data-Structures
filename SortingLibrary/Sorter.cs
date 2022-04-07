4cbc9fa92e728a577133cca253f88de5e3b9d3c8
Tobie Allen <tallen@student.neumont.edu>
2022-04-07 12:18:31 -0600

    bubble and insertion done and working

--- SortingLibrary/Sorter.cs
+++ SortingLibrary/Sorter.cs
@@ -10,15 +10,18 @@ namespace SortingLibrary
     {
         public static void BubbleSort(T[] arr)
         {
+            T temp; 
             //arr[0].CompareTo(arr[1]) > 0 //this is how to compare 2 indexes when you don't know the data type 
-            for (int i = 0; i < arr.Length; i++)
+            for (int i = arr.Length; i >= 1; i--)
             {
-                for (int j = 0; j < arr.Length; j++)
+                for (int j = 0; j < i - 1; j++)
                 {
                     if (arr[j].CompareTo(arr[j + 1]) > 0)
                     {
                         //swap the values 
-
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp; 
                     }
                 }                
             }
@@ -26,15 +29,20 @@ namespace SortingLibrary
 
         public static void InsertionSort(T[] arr)
         {
-
-            for (int i = 0; i < arr.Length; i++)
-            {
+            T temp;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {                
                 // need to pull out number that is being compared to others and track it 
                 // if switches, the number it switched with overwrites the original number 
                 // stable swap, means if number is equal to number it's being compared to, don't swap 
-                for (int j = 0; j < arr.Length; j++)
+                for (int j = i + 1; j > 0; j--)
                 {
-
+                    if (arr[j - 1].CompareTo(arr[j]) > 0)
+                    {
+                        temp = arr[j - 1];
+                        arr[j - 1] = arr[j];
+                        arr[j] = temp; 
+                    }
                 }
             }
         }
