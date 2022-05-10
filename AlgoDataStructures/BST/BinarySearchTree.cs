b61714265a49144ab50a15f80c4c3e8a81f75bc4
Tobie Allen <tallen@student.neumont.edu>
2022-05-10 09:07:54 -0600

    Binary search tree started

---
+++ AlgoDataStructures/BST/BinarySearchTree.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoDataStructures.BST
+{
+    public class BinarySearchTree<T> where T :IComparable
+    {
+        public BinaryTreeNode<T> Root { get; set; }
+    }
+}
